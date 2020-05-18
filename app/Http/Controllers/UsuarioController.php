0d71164542b15fc471829def0ae5521078bcca39
AlexLuna-a <64175377+AlexLuna-a@users.noreply.github.com>
2020-05-17 19:22:31 -0500

    Correccion de CRUD de usuario (no actualizaba con update) - Se agrego la validación isLog para usuario y se crearon los helpers

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -10,55 +10,66 @@ class UsuarioController extends Controller {
 
     private $usuario;
 
+    //inicializa objeto usuario
     function __construct() {
         $this->usuario = new Usuario();
     }
 
+    //setea los valores a usuario
     public function setUsuario() {
         if (session()->exists('usuario')) {
             $this->usuario->id = session('usuario')->id;
             $this->usuario->password_user = session('usuario')->password_user;
-            $this->usuario->apellido_p_user = session('usuario')->apellido_p_user ;
+            $this->usuario->apellido_p_user = session('usuario')->apellido_p_user;
             $this->usuario->apellido_m_user = session('usuario')->apellido_m_user;
             $this->usuario->nombre_user = session('usuario')->nombre_user;
             $this->usuario->clave_tipo = session('usuario')->clave_tipo;
             $this->usuario->fecha_ingreso_user = session('usuario')->fecha_ingreso_user;
-            $this->usuario->vigencia_user = session('usuario')->vigencia_user ;
+            $this->usuario->vigencia_user = session('usuario')->vigencia_user;
         }
     }
+
+    //seteta a un usuario por parametro
     public function setUsuariotemp($usuario) {
-            $this->usuario->id = $usuario->id;
-            $this->usuario->password_user = $usuario->password_user;
-            $this->usuario->apellido_p_user = $usuario->apellido_p_user ;
-            $this->usuario->apellido_m_user = $usuario->apellido_m_user;
-            $this->usuario->nombre_user = $usuario->nombre_user;
-            $this->usuario->clave_tipo = $usuario->clave_tipo;
-            $this->usuario->fecha_ingreso_user = $usuario->fecha_ingreso_user;
-            $this->usuario->vigencia_user = $usuario->vigencia_user ;
+        $this->usuario->id = $usuario->id;
+        $this->usuario->password_user = $usuario->password_user;
+        $this->usuario->apellido_p_user = $usuario->apellido_p_user;
+        $this->usuario->apellido_m_user = $usuario->apellido_m_user;
+        $this->usuario->nombre_user = $usuario->nombre_user;
+        $this->usuario->clave_tipo = $usuario->clave_tipo;
+        $this->usuario->fecha_ingreso_user = $usuario->fecha_ingreso_user;
+        $this->usuario->vigencia_user = $usuario->vigencia_user;
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    //Valida el codigo
+    public function ValidCode($codigo) {
+        for ($i = 0; $i < strlen($codigo); $i++) {
+            if (!is_numeric($codigo[$i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //vista de loggin
     public function loggin() {
         return view('Usuario.loggin');
     }
 
     public function loggout() {
+        if (!isLog()) {
+            return redirect()->action('usuarioController@loggin');
+        }
         session()->flush();
         return redirect()->action('usuarioController@index');
     }
-        
-    public function loggin_in(Request $request) {
 
+    public function loggin_in(Request $request) {
         $codigo = $request->input('codigo_user');
         $pass = $request->input('password_user');
-        $usuario = $this->usuario->findUsuario($codigo);
+        $usuario = $this->usuario->find($codigo);
 
         if ($usuario) {
-
             if ($codigo == $usuario->id &&
                     $pass == $usuario->password_user) {
                 session()->put(['usuario' => $usuario]);
@@ -66,16 +77,12 @@ class UsuarioController extends Controller {
                 session()->put(['nivel' => $this->usuario->tipo->nivel_tipo]);
                 return redirect()->action('usuarioController@index');
             } else {
-
                 $mensaje = 'el usuario y/o la contraseña no coinciden, intentalo de nuevo';
             }
         } else {
-
             $mensaje = 'no se reconoce el usuario';
         }
-
         if ($mensaje) {
-
             session()->flash('mensaje', $mensaje);
             return redirect()->action('usuarioController@loggin');
         }
@@ -84,15 +91,19 @@ class UsuarioController extends Controller {
     public function reg() {
         $tipHelp = new tipoUsuario();
         $tipos = $tipHelp->getDisponibles();
+        if (!session()->exists('vigencia')) {
+            $vigencia = date("y-m-d", strtotime('last day of December', time()));
+            session()->put('vigencia', $fecha);
+        }
         return view('Usuario.formCU', ['tipos' => $tipos]);
     }
 
     public function reg_in(Request $request) {
         $codigo = $request->input('codigo_user');
-        if (session()->exists('usuario')) {
-            if ($codigo != session('usuario')->id) {
+        if (isLog()) {
+            if ($codigo != session('usuario')->id){
                 $usuario = $this->usuario->findUsuario($codigo);
-                if (is_object($usuario)){
+                if (is_object($usuario)) {
                     session()->flash('mensajeError', 'Error! El codigo le pertenece a otro usuario');
                     return redirect()->action('usuarioController@edit');
                 }
@@ -104,6 +115,8 @@ class UsuarioController extends Controller {
         $apellidoM = $request->input('apellidoM');
         $nombre = $request->input('nombre');
         $tipo = $request->input('tipo');
+        $ingreso = $request->input('ingreso');
+        $vigencia= $request->input('vigencia');
 
         $this->usuario->id = $codigo;
         $this->usuario->password_user = $pass;
@@ -111,26 +124,21 @@ class UsuarioController extends Controller {
         $this->usuario->apellido_m_user = $apellidoM;
         $this->usuario->nombre_user = $nombre;
         $this->usuario->clave_tipo = $tipo;
+        $this->usuario->fecha_ingreso_user = $ingreso;
+        $this->usuario->vigencia_user = $vigencia;
+        
+        
 
+        if (isLog()) {
+            $this->usuario->acttualizar();
 
-        if (session()->exists('usuario')) {
-            session()->flush();
-            $this->usuario->update();
+            $usuario = $this->usuario->find($codigo);
             
-
-            $usuario = $this->usuario->findUsuario($codigo);
             session()->put(['usuario' => $usuario]);
 
             session()->flash('mensajeCompleto', 'Se han guardado los cambios');
             return redirect()->action('usuarioController@edit');
         } else {
-
-
-            $fecha = date("y-m-d");
-            $this->usuario->fecha_ingreso_user = $fecha;
-            $helpDate = strtotime('last day of December', time());
-            $fecha = date("y-m-d", $helpDate);
-            $this->usuario->vigencia_user = $fecha;
 
             $this->usuario->save();
 
@@ -141,14 +149,16 @@ class UsuarioController extends Controller {
         }
     }
 
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index() {
-        if (session()->exists('usuario')) {
-            
-            return view('Usuario.index');
-            
-        } else {
+        if (!isLog()) {
             return redirect()->action('usuarioController@loggin');
         }
+        return view('Usuario.index');
     }
 
     /**
@@ -187,6 +197,9 @@ class UsuarioController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function edit() {
+        if (!isLog()) {
+            return redirect()->action('usuarioController@loggin');
+        }
         $tipHelp = new tipoUsuario();
         $tipos = $tipHelp->getDisponibles(session('nivel'));
         $usuario = session('usuario');
