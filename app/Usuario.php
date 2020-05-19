92c08d978d51f6113b83eabfff8b0b10245aa8c4
AlexLuna-a <64175377+AlexLuna-a@users.noreply.github.com>
2020-05-18 22:37:31 -0500

    redefinicion de modelo usuario

--- app/Usuario.php
+++ app/Usuario.php
@@ -3,51 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
-class Usuario extends Model {
-
-    protected $fillable= [
-        'id', 'apellido_p_user', 'apellido_m_user', 'nombre_user','fecha_ingreso_user' ,'vigencia_user', 'clave_tipo'
-    ];
-    protected $hidden =['password_user'];
-    protected $table;
-    function __construct() {
-        $this->table = 'usuario';
-    }
-
-    public function findUsuario($codigo) {
-        $tabla = $this->getTable();
-
-        $usuario = DB::table($tabla)->where('id', '=', $codigo)->first();
-
-        return $usuario;
-    }
+class Usuario extends Model
+{
+    //public $timestamps = false;
+    protected $table = 'usuario';
+    protected $keyType = 'string';
     
     
-    public function acttualizar(){
-        $tabla = $this->getTable();
-        
-        $usuario= DB::table($tabla)->where('id','=', $this->id)
-                ->update( array(
-                    'apellido_p_user' => $this->apellido_p_user, 
-                    'apellido_m_user' => $this->apellido_m_user,
-                    'nombre_user' => $this->nombre_user, 
-                    'vigencia_user'=> $this->vigencia_user, 
-                    'clave_tipo' => $this->clave_tipo,
-                    'fecha_ingreso_user' => $this->fecha_ingreso_user
-                    
-                ));
-                
-    }
-    
-
     
     public function tipo(){
-        return $this->belongsTo('\App\tipoUsuario','clave_tipo');
-        
+        return $this->belongsTo('App\TipoUsuario', 'clave_tipo');
     }
-    
-    
-
 }
